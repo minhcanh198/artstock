9a29ae205ad0633653bd5131388d28268aa9c3b9
Nguyen Minh Canh <silverben98@gmail.com>
2022-05-21 23:39:33 +0700

    fix null user

--- app/Models/Purchases.php
+++ app/Models/Purchases.php
@@ -7,15 +7,17 @@ use Illuminate\Http\Request;
 
 class Purchases extends Model
 {
-  protected $guarded = array();
-  public $timestamps = false;
+    protected $guarded = array();
+    public $timestamps = false;
 
-  public function user() {
-    return $this->belongsTo('App\Models\User')->first();
-  }
+    public function user()
+    {
+        return $this->belongsTo('App\Models\User', 'user_id', 'id');
+    }
 
-  public function images(){
-    return $this->belongsTo('App\Models\Images')->first();
-  }
+    public function images()
+    {
+        return $this->belongsTo('App\Models\Images')->first();
+    }
 
 }
