f623b0c6399f0160f470bb3d4d5e91367ab605f3
Nguyen Minh Canh <silverben98@gmail.com>
2022-10-17 00:54:55 +0700

    update css

--- resources/views/users/profile.blade.php
+++ resources/views/users/profile.blade.php
@@ -114,7 +114,7 @@ if (Auth::check()) {
                 <div class="text-center">
                     <a href="{{ url($user->username) }}" class="w-100 d-flex justify-content-center">
                         <img loading="lazy" src="{{ asset('avatar').'/'.$user->avatar }}" width="150" height="150"
-                             class="img-circle border-avatar-profile avatarUser"/>
+                             class="img-circle border-avatar-profile avatarUser img-object-center"/>
                     </a>
                 </div>
 
